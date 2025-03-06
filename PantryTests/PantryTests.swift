097ab9e0005e36c690fe6a75445c8802a5376767
Joseph O'Brien <jmobrien127@gmail.com>
2025-03-06 04:27:48 -0700

    test: Add test targets
    - Add unit test target - Add UI test target with launch test

---
+++ PantryTests/PantryTests.swift
@@ -0,0 +1,16 @@
+//
+//  PantryTests.swift
+//  PantryTests
+//
+//  Created by Joseph O'Brien on 3/6/25.
+//
+
+import Testing
+
+struct PantryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
