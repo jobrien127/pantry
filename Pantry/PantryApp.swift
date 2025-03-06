f09cb4d53a9da6d5411bfb46da791b78744aff7c
Joseph O'Brien <jmobrien127@gmail.com>
2025-03-06 04:27:18 -0700

    chore: Initial project setup with Xcode project files

---
+++ Pantry/PantryApp.swift
@@ -0,0 +1,32 @@
+//
+//  PantryApp.swift
+//  Pantry
+//
+//  Created by Joseph O'Brien on 3/6/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PantryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView(modelContext: sharedModelContainer.mainContext)
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
